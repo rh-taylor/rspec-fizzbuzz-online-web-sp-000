808c080ebb2a69aa304ac17d988e407788c9c22b
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-03 23:13:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(x)
-  if x % 3 == 0
+def fizzbuzz(int)
+  if int % 3 == 0
     puts "Fizz"
-  elsif x % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
-  elsif x % 3 == 0 && x % 5 == 0
+  elsif int % 3 == 0 && x % 5 == 0
     puts "FizzBuzz"
   else
     puts nil
