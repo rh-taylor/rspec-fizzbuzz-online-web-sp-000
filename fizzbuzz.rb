0ebd42ce0cb993ee986446e402dc8fa874ac7d8f
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-03 23:19:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,9 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(x)
-  
-  if x % 3 == 0
+  if x % 3 == 0 && x % 5 == 0
+    puts "FizzBuzz"
+  elsif x % 3 == 0
     puts "Fizz"
   elsif x % 5 == 0
     puts "Buzz"
