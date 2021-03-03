f887a3876f7a820154a63a64657c9a552d8e9323
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-03 23:19:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -14,3 +14,14 @@ def fizzbuzz(x)
 end
 
 
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 ==0
+      "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  else
+    puts nil
+  end
+end
