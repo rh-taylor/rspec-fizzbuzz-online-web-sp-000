0bdb3a9e601c10bf671dc20a2b237b06173dd67d
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-03 23:12:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(x)
+  if x % 3 == 0
+    puts "Fizz"
+  elsif x % 5 == 0
+    puts "Buzz"
+  elsif x % 3 == 0 && x % 5 == 0
+    puts "FizzBuzz"
+  else
+    puts nil
+  end
+fizz_3 = fizzbuzz(3)
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
+end
